721e11dfb08c5252548537e0c965bd2399daac85
Preety <sethy.preety@gmail.com>
2025-03-30 13:23:20 +0400

    Beginners guide

---
+++ HelloWorld.js
@@ -0,0 +1,10 @@
+<html>
+    <head> </head>
+        <body>
+            <h1>Welcome to 30 days of JavaScript</h1>
+            <script>
+                console.log("Hello World !!");
+            </script>
+        </body>
+   
+</html>
