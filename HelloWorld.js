3cbeda2c96853d03f0ddc23d955cf5027ee0694b
Preety <sethy.preety@gmail.com>
2025-04-04 19:06:59 +0400

    Only comments added git add HelloWorld.js

--- HelloWorld.js
+++ HelloWorld.js
@@ -4,6 +4,7 @@
             <h1>Welcome to 30 days of JavaScript</h1>
             <script>
                 console.log("Hello World !!");
+                console.log("Welcome to the world of JavaScript !!");
             </script>
         </body>
    
